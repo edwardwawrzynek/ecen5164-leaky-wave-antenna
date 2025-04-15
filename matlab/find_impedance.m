5245b59a56663839e64b6f5e5e6c8d5b5140b14c
aikelheimer <111887672+aikelheimer@users.noreply.github.com>
2025-04-15 15:16:24 -0600

    Add files via upload
    File for finding surface impedance from input impedance

---
+++ matlab/find_impedance.m
@@ -0,0 +1,32 @@
+clear; close all;
+
+f0 = 10e9;
+gamma = 0.8613+1j*0.4836;
+
+e0 = 8.854*10^-12;
+u0 = 4*pi*10^-7;
+
+er1 = 6.15;
+c0 = 1/sqrt(e0*u0);
+lambda0 = c0/f0;
+
+eta0 = sqrt(u0/e0);
+k0 = 2*pi/lambda0;
+k1 = k0*sqrt(er1);
+space = 0.0174425;
+gammaD = gamma*exp(2j*k0*space);
+
+etaIn = eta0*(1+gammaD)/(1-gammaD);
+
+eta1 = eta0/sqrt(er1);
+d = 2.54*10^-3; %mm
+
+
+eta_sheet = ((1/etaIn)-(1/(1j*eta1*tan(k1*d))))^-1;
+
+%% Numerical Solution
+syms f(etaSurf)
+
+kx1(etaSurf) = sqrt(k0^2*(er1-1)+(etaSurf*k0/eta0)^2);
+f(etaSurf) = -(1/etaSurf)+(1/eta_sheet)+(1/(1j*tan(kx1*d)*eta0*kx1/(k0*er1)));
+vpasolve(f)
