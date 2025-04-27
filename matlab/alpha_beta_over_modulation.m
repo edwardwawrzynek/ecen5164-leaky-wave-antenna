139e6b11f84e3705eb4bdfeb3cec570be782dfab
Anna <aikelheimer@gmail.com>
2025-04-27 14:03:29 -0600

    new sim and NO results :(

--- matlab/alpha_beta_over_modulation.m
+++ matlab/alpha_beta_over_modulation.m
@@ -3,12 +3,37 @@ X = 1.2;
 k0_a = 5.917;
 M = 0:0.01:1;
 
-kappa_over_k0 = sqrt(1+X^2) - ((M.^2)/4)*((X^2)/sqrt(1+X^2))*(1/(1-(1i/X)*sqrt(1+2*pi/k0_a-sqrt(1+X^2)))+1/(1-(1i/X)*sqrt(1-2*pi/k0_a-sqrt(1+X^2))))
+kappa_over_k0 = sqrt(1+X^2) - ((M.^2)/4)*((X^2)/sqrt(1+X^ ...
+    2))*(1/(1-(1i/X)*sqrt(1+2*pi/k0_a-sqrt(1+X^2)))+1/(1-(1i/ ...
+    X)*sqrt(1-2*pi/k0_a-sqrt(1+X^2))));
 
 beta_over_k0 = real(kappa_over_k0);
 alpha_over_k0 = -imag(kappa_over_k0);
 
-figure(1)
-plot(M, beta_over_k0)
-figure(2)
-plot(M, alpha_over_k0)
+% figure(1)
+% plot(M, beta_over_k0)
+% figure(2)
+% plot(M, alpha_over_k0)
+
+u0=4*pi*10^-7;
+e0=8.854e-12;
+eta0=sqrt(u0/e0);
+
+M = 0.2;
+a = 28.25e-3;
+Xprime = 1.2;
+z = linspace(0,28.25e-3,10);
+
+X0 = eta0.*Xprime.*(1+M.*cos(z.*2*pi./a));
+
+plot(10*z/a,X0)
+
+gap_size_um = (-1.9667*X.^5 + 1943.9703*X.^4 + -390156.861*X.^ ...
+    3 + -4674.6409*X.^2 + -26.3167*X + -0.011268) ./ (X.^4 + -466.5891*X.^ ...
+    3 + 52195.9033*X.^2 + -15293.5661*X + -166.5399);
+
+gap_size_um1 = (-1.9667*X0.^5 + 1943.9703*X0.^4 + -390156.861*X0.^ ...
+    3 + -4674.6409*X0.^2 + -26.3167*X0 + -0.011268) ./ (X0.^4 + -466.5891*X0.^ ...
+    3 + 52195.9033*X0.^2 + -15293.5661*X0 + -166.5399);
+gap_size_um1 = gap_size_um1';
+plot(10*z/a,gap_size_um1)
